6ec784339cd9688b0eab749f42458d7ed8465bca
Justine Leleu <justine.leleu.28@gmail.com>
2023-09-25 15:35:16 +0200

    test push

--- assets/script/burgerClass.js
+++ assets/script/burgerClass.js
@@ -15,9 +15,6 @@ let fish2 = new Burger("fish", "Spicy Oceanic Delight", "../images/burger_fish2.
 let fish3 = new Burger("fish", "Land and Sea Fusion Burger", "../images/burger_fish4.jpeg", "A combination of grilled shrimp and fish fillet, featuring seaweed salad, marinated cucumber, wasabi mayonnaise, and pickled ginger, all served in a fresh bun", 10.99);
 let fish4 = new Burger("fish", "Tropical Breeze Burger", "../images/burger_fish1.jpeg", "Grilled fish fillet with a tropical twist, including two slices of fish, fresh mango, coconut lettuce, and exotic citrus sauce, all wrapped in a fresh burger bun", 13.99);
 
-
-
-
 let veggie1 = new Burger("veggie", "Supreme Courgette","./images/burger_veggie1.jpeg", "Crispy burger with veggie steak and courgettes", 12);
 let veggie2 = new Burger("veggie", "Vegan Cheese","./images/burger_veggie2.jpeg", "Cheese burger with veggie steak", 12);
 let veggie3 = new Burger("veggie", "Paprika Crispy","./images/burger_veggie3.jpeg", "Crispy burger with veggie steak and paprika", 12);
@@ -28,11 +25,10 @@ let chicken2 = new Burger("chicken", "Chicken Filet", "../images/burger_chicken1
 let chicken3 = new Burger("chicken", "Triple Chicken", "../images/burger_chicken4.jpeg", "A triple layer of chicken separated by layers of cheddar and salad", 20);
 let chicken4 = new Burger("chicken", "Nugget Burger", "../images/burger_chicken2.jpeg", "Pieces of chicken nuggets with salad and onions", 20);
 
- let beef1 = new Burger("beef", "Big Tom", "..\assets\images\burger_beef1.jpeg", "the original big tom burgers with his secret sauce", 8.99);
+let beef1 = new Burger("beef", "Big Tom", "..\assets\images\burger_beef1.jpeg", "the original big tom burgers with his secret sauce", 8.99);
 
- let beef2 = new Burger("beef", "Double Impact", "..\assets\images\burger_beef2.jpeg", "a double grilled beef burger with extra bacon and our special 2 sauces mix", 11.99);
+let beef2 = new Burger("beef", "Double Impact", "..\assets\images\burger_beef2.jpeg", "a double grilled beef burger with extra bacon and our special 2 sauces mix", 11.99);
 
- let beef3 = new Burger("beef", "Super Cheese", "..\assets\images\burger_beef3.jpeg", "the original beef with extra raclette cheese and mushrooms", 11.99);
+let beef3 = new Burger("beef", "Super Cheese", "..\assets\images\burger_beef3.jpeg", "the original beef with extra raclette cheese and mushrooms", 11.99);
 
- let beef4 = new Burger("beef", "Dark Bob", "..\assets\images\burger_beef4.jpeg", "A double beef burger with cheese and a darklicious roasted bun", 13.99);
-
+let beef4 = new Burger("beef", "Dark Bob", "..\assets\images\burger_beef4.jpeg", "A double beef burger with cheese and a darklicious roasted bun", 13.99);
