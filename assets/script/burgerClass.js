d5f4f9dfd46eaa69fe4d0023758c248eb4f2da18
Justine Leleu <justine.leleu.28@gmail.com>
2023-09-25 15:11:39 +0200

    adding chicken burgers objects

--- assets/script/burgerClass.js
+++ assets/script/burgerClass.js
@@ -9,3 +9,8 @@ class Burger
         this.price = price;
     }
 }
+
+let chicken1 = Burger("chicken", "Chicken & Veggies", "../images/burger_chicken3.jpeg", "Chicken with veggies of all sorts, cucumbers, tomatoes, eggplant and salad", 20);
+let chicken2 = Burger("chicken", "Chicken Filet", "../images/burger_chicken1.jpeg", "Soft bread, pepper mayonnaise, salad, onions and a delicious crusty chicken filet", 20);
+let chicken3 = Burger("chicken", "Triple Chicken", "../images/burger_chicken4.jpeg", "A triple layer of chicken separated by layers of cheddar and salad", 20);
+let chicken4 = Burger("chicken", "Nugget Burger", "../images/burger_chicken2.jpeg", "Pieces of chicken nuggets with salad and onions", 20);
