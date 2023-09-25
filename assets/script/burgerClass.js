db8734468ac05e862ef3f96b43b6f50c6e98fc1e
tomboszko <gomreethomas@gmail.com>
2023-09-25 15:05:17 +0200

    added vairables burger class

--- assets/script/burgerClass.js
+++ assets/script/burgerClass.js
@@ -9,3 +9,13 @@ class Burger
         this.price = price;
     }
 }
+
+
+ let beef1 = new Burger("beef", "Big Tom", "assets\images\burger_beef1.jpeg", "the original big tom burgers with his secret sauce", 8.99);
+
+ let beef2 = new Burger("beef", "Double Impact", "assets\images\burger_beef2.jpeg", "a double grilled beef burger with extra bacon and our special 2 sauces mix", 11.99);
+
+ let beef3 = new Burger("beef", "Super Cheese", "assets\images\burger_beef3.jpeg", "the original beef with extra raclette cheese and mushrooms", 11.99);
+
+ let beef4 = new Burger("beef", "Dark Bob", "assets\images\burger_beef4.jpeg", "A double beef burger with cheese and a darklicious roasted bun", 13.99);
+
