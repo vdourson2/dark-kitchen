29c47b9be0bab84714b427a7a7362924595ab07c
Justine Leleu <justine.leleu.28@gmail.com>
2023-09-25 14:37:05 +0200

    adding script js

---
+++ assets/script/burgerClass.js
@@ -0,0 +1,11 @@
+class Burger
+{
+    constructor(category, burgerName, illustration, description, price)
+    {
+        this.category = category;
+        this.burgerName = burgerName;
+        this.illustration = illustration;
+        this.description = description;
+        this.price = price;
+    }
+}
