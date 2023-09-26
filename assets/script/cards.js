53f71eb45fe4c0a6e29c51f08c442f755a20eb8b
Virginie Dourson <virginiedourson@gmail.com>
2023-09-26 15:32:03 +0200

    Finish cards

--- assets/script/cards.js
+++ assets/script/cards.js
@@ -1,7 +1,9 @@
 let selectedBurgers = [];
 
-function addCart(e,burger) {
+function addCart(burger) {
     selectedBurgers.push(burger);
+    console.log(selectedBurgers);
+    addElementToCart();
 }
 
 let cards = document.querySelector(".cards");
@@ -9,13 +11,16 @@ for (let burger of burgers) {
     let wrapBurger = document.createElement("div");
     wrapBurger.classList.add("cards__wrapBurger");
     cards.appendChild(wrapBurger);
-    wrapBurger.innerHTML = `<img class="cards__wrapBurger__illustration" src="${burger.illustration}" alt="Burger picture">
-                            <h2 class="cards__wrapBurger__burgerName">${burger.burgerName}</h2>
-                            <p class="cards__wrapBurger__description">${burger.description}</p>
-                            <div class="cards__wrapBurger__wrapPrice">
-                                <p class="cards__wrapBurger__wrapPrice__price">${burger.price}<span class="cards__wrapBurger__wrapPrice__price__euro"> €</span></p>
-                                <div class="cards__wrapBurger__wrapPrice__add"> + </div>
+    wrapBurger.innerHTML = `<div class="cards__wrapBurger__black"></div>
+                            <img class="cards__wrapBurger__illustration" src="${burger.illustration}" alt="Burger picture">
+                            <div class="cards__wrapBurger__white">
+                                <h2 class="cards__wrapBurger__burgerName">${burger.burgerName}</h2>
+                                <p class="cards__wrapBurger__description">${burger.description}</p>
+                                <div class="cards__wrapBurger__wrapPrice">
+                                    <p class="cards__wrapBurger__wrapPrice__price">${burger.price}<span class="cards__wrapBurger__wrapPrice__price__euro"> €</span></p>
+                                    <div class="cards__wrapBurger__wrapPrice__add"> + </div>
+                                </div>
                             </div>`;
     let add = wrapBurger.querySelector(".cards__wrapBurger__wrapPrice__add");
-    add.addEventListener('click', (e) => addCart(e,burger));
+    add.addEventListener('click', () => addCart(burger));
 }
