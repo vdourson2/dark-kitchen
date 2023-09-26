02d7ed9f845085962d9bc489608cc9adedd19409
Justin Michel <justinmichel2000@gmail.com>
2023-09-26 15:35:31 +0200

    test

---
+++ assets/script/shoppingCart.js
@@ -0,0 +1,26 @@
+function openCart(params) {
+
+    document.get
+
+
+
+
+    
+}
+
+function closeCart(){
+
+
+
+
+}
+function setUp() {
+    document.getElementById("menu").onclick = setMenu;
+}
+
+function setMenu() {
+    var menuBox = document.getElementById("menu-box");
+    menuBox.className = "menuBox";   
+}
+
+setUp();
